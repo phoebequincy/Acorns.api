243923b433dce2b06fd53a1c19dd1996a79595c3
Phoebe Quincy <phoebequincy@gmail.com>
2019-03-08 11:33:28 -0700

    Building out routes, added users and acorns routes. GET working.

--- routes/acorns.js
+++ routes/acorns.js
@@ -1,61 +1,18 @@
-'use strict'
+'use strict';
 
 const express = require('express')
-const knex = require('../knex.js')
-const router = express.Router();
+const knex = require('../knex')
+const router = express.Router()
 
-// router.get('/', function(req, res, next) {
-//   return knex('acorns')
-//   .select('id', 'title', 'acorns')
-//   .then ((result) => {
-//     res.status(200).json(result)
-//   })
-// })
-//
-// router.get('/:id', function(req, res, next) {
-//   return knex('acorns')
-//   .where('id', req.params.id)
-//   .select('id', 'title', 'acorns')
-//   .first()
-//   .then(data => {
-//     res.status(200).json(data)
-//   })
-// })
-//
-// router.post('/', function(req, res, next) {
-//   knex('acorns')
-//   .insert({
-//     name: req.body.name,
-//     message: req.body.message
-//   })
-//   .returning(['id','title', 'acorns'])
-//   .then(data => {
-//     res.status(200).json(data[0])
-//   })
-// })
-//
-// router.patch('/:id', function(req, res, next) {
-//   knex('acorns')
-//   .where('id', req.params.id)
-//   .update(req.body)
-//   .returning(['id','title', 'acorns'])
-//   .then(data => {
-//     res.send(data[0])
-//   })
-// })
-//
-// router.delete('/:id', function(req, res, next) {
-//   knex('acorns')
-//   .where('id', req.params.id)
-//   .returning(['id','title', 'acorns'])
-//   .del()
-//   .then(data => {
-//     if (!data) {
-//       res.send("not found")
-//     } else {
-//       res.status(200).json(data[0])
-//     }
-//   })
-// })
+router.get('/', (req, res, next) => {
+  console.log("acorns")
+  return knex('acorns')
+    .then((acorns) => {
+      res.status(200).send(acorns)
+    })
+    .catch((err) => {
+    next(err)
+  })
+})
 
 module.exports = router;
