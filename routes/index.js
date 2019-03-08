0d82dde92d9b2887eb2bb82a34b5073647250c66
Phoebe Quincy <phoebequincy@gmail.com>
2019-03-08 13:06:37 -0700

    Updated nodemon and fixed a typo in index.js on the GET route.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,63 @@
-var express = require('express');
-var router = express.Router();
+'use strict'
 
-/* GET home page. */
+const express = require('express')
+const knex = require('../knex.js')
+const router = express.Router();
+
+//get all
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  return knex('acorns')
+  .select('id', 'title', 'category', 'content')
+  .then ((result) => {
+    res.status(200).json(result)
+  })
+})
+
+//get one
+router.get('/:id', function(req, res, next) {
+  return knex('acorns')
+  .where('id', req.params.id)
+  .select('id', 'title', 'category', 'content')
+  .first()
+  .then(data => {
+    res.status(200).json(data)
+  })
+})
+
+router.post('/', function(req, res, next) {
+  knex('acorns')
+  .insert({
+    name: req.body.name,
+    message: req.body.message
+  })
+  .returning(['id', 'title', 'category', 'content'])
+  .then(data => {
+    res.status(200).json(data[0])
+  })
+})
+
+router.patch('/:id', function(req, res, next) {
+  knex('acorns')
+  .where('id', req.params.id)
+  .update(req.body)
+  .returning(['id', 'title', 'category', 'content'])
+  .then(data => {
+    res.send(data[0])
+  })
+})
+
+router.delete('/:id', function(req, res, next) {
+  knex('acorns')
+  .where('id', req.params.id)
+  .returning(['id', 'title', 'category', 'content'])
+  .del()
+  .then(data => {
+    if (!data) {
+      res.send("not found")
+    } else {
+      res.status(200).json(data[0])
+    }
+  })
+})
 
 module.exports = router;
