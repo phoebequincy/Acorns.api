ec91ca08c2d642733ecc124970ab5f965ddb07b0
Phoebe Quincy <phoebequincy@gmail.com>
2019-03-08 21:12:12 -0700

    Added cors and set up Acorns get one route.

--- routes/acorns.js
+++ routes/acorns.js
@@ -4,14 +4,60 @@ const express = require('express')
 const knex = require('../knex')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-  console.log("acorns")
+
+//get all
+router.get('/', function(req, res, next) {
   return knex('acorns')
-    .then((acorns) => {
-      res.status(200).send(acorns)
-    })
-    .catch((err) => {
-    next(err)
+  .select('id', 'title', 'category', 'content')
+  .then ((result) => {
+    res.status(200).json(result)
+  })
+})
+
+//get one
+router.get('/:id', function(req, res, next) {
+  return knex('acorns')
+  .where('id', req.params.id)
+  .select('id', 'title', 'category', 'content')
+  .first()
+  .then(data => {
+    res.status(200).json(data)
+  })
+})
+
+router.post('/', function(req, res, next) {
+  knex('acorns')
+  .insert({
+    name: req.body.name,
+    message: req.body.message
+  })
+  .returning(['id', 'title', 'category', 'content'])
+  .then(data => {
+    res.status(200).json(data[0])
+  })
+})
+
+router.patch('/:id', function(req, res, next) {
+  knex('acorns')
+  .where('id', req.params.id)
+  .update(req.body)
+  .returning(['id', 'title', 'category', 'content'])
+  .then(data => {
+    res.send(data[0])
+  })
+})
+
+router.delete('/:id', function(req, res, next) {
+  knex('acorns')
+  .where('id', req.params.id)
+  .returning(['id', 'title', 'category', 'content'])
+  .del()
+  .then(data => {
+    if (!data) {
+      res.send("not found")
+    } else {
+      res.status(200).json(data[0])
+    }
   })
 })
 
