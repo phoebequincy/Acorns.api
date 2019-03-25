5c98b22954256912f6a1b56224148b25f6d39111
Phoebe Quincy <phoebequincy@gmail.com>
2019-03-24 19:17:16 -0600

    Trying to fix CORS, 404 and JSON errors.

--- routes/index.js
+++ routes/index.js
@@ -24,23 +24,6 @@ router.get('/:id', function(req, res, next) {
   })
 })
 
-router.post('/api/messages', (req, res) => {
-  res.header('Content-Type', 'application/json');
-  client.messages
-    .create({
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to: req.body.to,
-      body: req.body.body
-    })
-    .then(() => {
-      res.send(JSON.stringify({ success: true }));
-    })
-    .catch(err => {
-      console.log(err);
-      res.send(JSON.stringify({ success: false }));
-    });
-});
-
 router.patch('/:id', function(req, res, next) {
   knex('acorns')
   .where('id', req.params.id)
---
+++ routes/messages.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const knex = require('../knex.js')
+const router = express.Router();
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+const client = require('twilio')(
+  process.env.TWILIO_ACCOUT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(pino);
+
+
+//full api url: 'https://api.twilio.com/2010-04-01/Accounts/{AccountSid}/Messages/{Sid}.json'
+app.post('/api/messages', (req, res) => {
+  res.header('Content-Type', 'application/json');
+  client.messages
+    .create({
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: req.body.to,
+      body: req.body.body
+    })
+    .then(() => {
+      res.send(JSON.stringify({ success: true }));
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(JSON.stringify({ success: false }));
+    });
+});
